2dc8070ed4c2105411a5316d01ecc7d900b20079
Alexander Cook <alex.chef.cook@gmail.com>
2019-05-13 06:08:48 +0000

    few changes from my new dev setup! exciting!

--- utilities.py
+++ utilities.py
@@ -35,9 +35,10 @@ def json_object_reddit(obj, subreddit, interval, requestType, cur):
 # Reddit search page
 
 def json_object_search(obj, subreddit, word, cur):
-    query = cur.execute("SELECT day, SUM(count) FROM (SELECT day, SUM(count) as count FROM reddit."+subreddit+" WHERE word ='"+word+"' GROUP BY day UNION SELECT generate_series(timestamp '2018-03-24', timestamp '"+datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day;")
+    startDate = str(datetime.datetime.now() - datetime.timedelta(days=365))
+    query = cur.execute("SELECT day, SUM(count) FROM (SELECT day, SUM(count) as count FROM reddit."+subreddit+" WHERE word ='"+word+"' GROUP BY day UNION SELECT generate_series(timestamp '"+startDate+"', timestamp '"+datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day;")
     results = cur.fetchall()
-    query = cur.execute("SELECT day, SUM(total) FROM (SELECT day, total FROM count_"+subreddit+" WHERE day >= '2018-03-24' UNION SELECT generate_series(timestamp '2018-03-24', timestamp '"+datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day;")
+    query = cur.execute("SELECT day, SUM(total) FROM (SELECT day, total FROM count_"+subreddit+" WHERE day >= '2018-03-24' UNION SELECT generate_series(timestamp '"+startDate+"', timestamp '"+datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day;")
     totals = cur.fetchall()
 
     i = 0
