517ae6f5951f6e32284c9e110fed0cf0fa97947d
Alex Cook <alex.chef.cook@gmail.com>
2020-08-24 18:19:40 -0700

    update to new timedelta

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 import psycopg2
 import datetime
-from datetime import datetime
+from datetime import datetime, date, time, timedelta
 import itertools
 from pytz import timezone
 import pytz
@@ -41,13 +41,13 @@ def json_object_reddit(obj, subreddit, interval, requestType, cur):
 # Reddit search page
 
 def json_object_search(obj, subreddit, word, cur):
-    startDate = str(datetime.datetime.now() - datetime.timedelta(days=365))
+    startDate = str(datetime.now() - timedelta(days=365))
 
     query = cur.execute("SELECT day, SUM(count) FROM (SELECT day, SUM(count) as count FROM reddit."+subreddit+" WHERE word ='"+word+"' GROUP BY day UNION SELECT generate_series(timestamp '" +
-                        startDate+"', timestamp '"+datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day LIMIT 365;")
+                        startDate+"', timestamp '"+datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day LIMIT 365;")
     results = cur.fetchall()
     query = cur.execute("SELECT day, SUM(total) FROM (SELECT day, total FROM count_"+subreddit+" UNION SELECT generate_series(timestamp '"+startDate+"', timestamp '" +
-                        datetime.datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day LIMIT 365;")
+                        datetime.now().strftime('%Y-%m-%d')+"', '1 day'):: DATE as day, 0) AS union_series GROUP BY union_series.day ORDER BY union_series.day LIMIT 365;")
     totals = cur.fetchall()
 
     i = 0
@@ -90,8 +90,7 @@ def json_object_events(obj, cur):
 # 2019 Superbowl page
 
 def json_object_time_series(obj, subject, cur):
-    query = cur.execute(
-        "SELECT interval::time, count,event FROM twitter."+subject+"  ORDER BY interval ASC;")
+    query = cur.execute( "SELECT interval::time, count,event FROM twitter."+subject+"  ORDER BY interval ASC;")
     results = cur.fetchall()
 
     i = 0
